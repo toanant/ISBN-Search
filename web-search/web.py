2e4b3564f37aabdfb2dc4ce7f14af5b8528c9de7
toanant <abhishek4bhopati@gmail.com>
2013-02-14 00:25:00 +0530

    long find results are fixed by applying limit & search on none value of author is fixed

--- web-search/web.py
+++ web-search/web.py
@@ -30,7 +30,7 @@ def detail(id):
 @app.route("/author/<author_name>/")
 def author_page(author_name):
     books = db.Details.find({"author":
-                             re.compile(author_name, re.IGNORECASE)})
+                             re.compile(author_name, re.IGNORECASE)}).limit(31)
     return render_template("results.html", books=books)
 
 @app.route("/publisher/<publisher_name>/")
@@ -38,58 +38,63 @@ def publisher_page(publisher_name):
 	publisher_name = str(publisher_name)
 	publisher_name = publisher_name.replace('/',' ')
 	books = db.Details.find({"Publisher":
-                             re.compile(publisher_name, re.IGNORECASE)})
+                             re.compile(publisher_name, re.IGNORECASE)}).limit(31)
 	return render_template("results.html", books=books)
 
 
 @app.route("/year/<year>/")
 def year_page(year):
     books = db.Details.find({"Publication Year":
-                             re.compile(year, re.IGNORECASE)})
+                             re.compile(year, re.IGNORECASE)}).limit(31)
     return render_template("results.html", books=books)
 
 
 @app.route("/search/", methods=["GET"])
 def search():
-	isbn = request.args.get("isbn")
-	keywords = request.args.get("keywords")
-	pattern_13 = re.compile(r"\d{13}")
-	_started_at = datetime.now()
-	if isbn:
-		if pattern_13.search(isbn):
-			isbn = pattern_13.search(isbn).group()
-			if(is_isbn13(isbn)):
-				isbn = isbn
-			else:
-				return render_template('Error.html')
-		else:
-			if(is_isbn10(isbn)):
-				isbn = isbn
-			else:
-				return render_template('Error.html')
-		size = len(isbn)
-		if size == 10:
-			detail = db.Details.find_one({'ISBN-10':isbn})
-			isbn = detail.get('_id')
-		else:
-			detail = db.Details.find_one({'_id': isbn})
-		review = db.Review.find_one({'_id': isbn})
-		if detail:
-			return render_template("details.html",
+    search = True
+    _started_at = datetime.now()
+    start = request.args.get("start")
+    limit = request.args.get("limit")
+    page = request.args.get("page")
+
+    isbn = request.args.get("isbn")
+    keywords = request.args.get("keywords")
+    pattern_13 = re.compile(r"\d{13}")
+    if isbn:
+        if pattern_13.search(isbn):
+            isbn = pattern_13.search(isbn).group()
+            if(is_isbn13(isbn)):
+                isbn = isbn
+            else:
+                return render_template('Error.html')
+        else:
+            if(is_isbn10(isbn)):
+                isbn = isbn
+            else:
+                return render_template('Error.html')
+        size = len(isbn)
+        if size == 10:
+            detail = db.Details.find_one({'ISBN-10':isbn})
+            isbn = detail.get('_id')
+        else:
+            detail = db.Details.find_one({'_id': isbn})
+        review = db.Review.find_one({'_id': isbn})
+        if detail:
+            return render_template("details.html",
                                    book=detail, review=review)
-		else:
-			return render_template("Error.html")
-	elif keywords:
-		if len(keywords) == 0:
-			return render_template("Error.html")
-		else:
-			books = db.Details.find({'keywords':re.compile(keywords, re.IGNORECASE)})
-			if (books.count() != 0):
-				return render_template("results.html",books=books)
-			else:
-				return render_template("Error.html")
-	else:
-		return " You must enter the query"
+        else:
+            return render_template("Error.html")
+    elif keywords:
+        if len(keywords) == 0:
+            return render_template("Error.html")
+        else:
+            books = db.Details.find({'keywords':re.compile(keywords, re.IGNORECASE)}).limit(37)
+            if (books.count() != 0):
+                return render_template("results.html",books=books)
+            else:
+                return render_template("Error.html")
+    else:
+        return render_template("Error.html")
 if __name__ == "__main__":
-	app.run(debug=True)
+    app.run(debug=True)
 
