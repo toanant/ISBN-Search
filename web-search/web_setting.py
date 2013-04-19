936f76efb3122712dea0a62df50f72094605b35a
toanant <abhishek4bhopati@gmail.com>
2013-04-19 20:55:11 +0530

    server side content files are deleted and new repo is set for that since this is public repo

--- web-search/web_setting.py
+++
@@ -1,8 +0,0 @@
-## This urlset contains website:search pattern in dictionary accordingly for website excluding flipkart
-urlset = {'flipkart': 'http://www.flipkart.com/search.php?query=',
-          'Infibeam': 'http://www.infibeam.com/Books/search?q=',
-          'Crossword': 'http://www.crossword.in/books/search?q=',
-          'Homeshop18': 'http://www.homeshop18.com/search:',
-          'Bookadda': 'http://www.bookadda.com/general-search?searchkey=',
-          'Rediffbook': 'http://books.rediff.com/book/ISBN:'}
-
